a6d75472c5c8964cd9cc532d73f134af9dbfa3ba
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-09-17 12:40:04 -0300

    fix: removing metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,26 @@
-'use client';
+"use client";
 
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Aside, Header, Modal } from "@/components";
-import { ModalProvider } from "@/contexts/modalContext";
+import { ModalProvider } from "@/contexts/ModalContext";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Contador App",
-  description: "O aplicativo do contador moderno",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="pt-BR">
-      
       <ModalProvider>
-        <body
-          className={`${inter.className} flex flex-col bg-gray-200`}
-        >
+        <body className={`${inter.className} flex flex-col bg-gray-200`}>
           <Modal />
           <Header />
           <main className="flex flex-col md:flex-row">
             <Aside />
-            <div className="flex-1 p-4 lg:p-6">
-              {children}
-            </div>
+            <div className="flex-1 p-4 lg:p-6">{children}</div>
           </main>
         </body>
       </ModalProvider>
